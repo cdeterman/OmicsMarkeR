a2782564dc5e288d98ca814696b0b93ad1bdb0d2
Charles Determan <cdetermanjr@hotmail.com>
2015-03-13 14:37:28 -0500

    relocate vignettes to proper location

--- vignettes/OmicsMarkeR.R
+++ vignettes/OmicsMarkeR.R
@@ -17,20 +17,20 @@
 library("OmicsMarkeR")
 set.seed(123)
 dat.discr <- create.discr.matrix(
-  create.corr.matrix(
-    create.random.matrix(
-      nvar = 50, 
-      ## the number of variables
-      nsamp = 100, 
-      ## the number of samples
-      st.dev = 1, 
-      ## the estimated standard deviation
-      perturb = 0.2
-      ## how noisy the data is
-      )),
-  D = 10
-  ## how many discriminating variables
-  )$discr.mat
+    create.corr.matrix(
+        create.random.matrix(
+            nvar = 50, 
+            ## the number of variables
+            nsamp = 100, 
+            ## the number of samples
+            st.dev = 1, 
+            ## the estimated standard deviation
+            perturb = 0.2
+            ## how noisy the data is
+        )),
+    D = 10
+    ## how many discriminating variables
+)$discr.mat
 
 
 ###################################################
@@ -51,7 +51,7 @@ results <- fs.stability(vars,
                         ## number of k-folds for cross-validation
                         verbose = FALSE
                         ## print output progress
-                        )
+)
 
 
 ###################################################
@@ -59,27 +59,3 @@ results <- fs.stability(vars,
 ###################################################
 performance.metrics(results)
 results$RPT
-
-###################################################
-### code chunk number 6: Alternate Tuning Resolutions
-###################################################
-grid <- denovo.grid(vars, "plsda", 15)
-grid <- append(grid, denovo.grid(vars, "svm", 3)) 
-
-# list of two different methods resolution
-
-results <- fs.stability(vars, 
-                        groups, 
-                        grid = grid,
-                        method = c("plsda", "svm"), 
-                        f = 10, 
-                        k = 3, 
-                        k.folds = 10, 
-                        verbose = FALSE)
-
-###################################################
-### code chunk number 7: Manual Tuning Resolutions
-###################################################
-grid <- list(expand.grid(.ncomp = seq(3,8)))
-grid <- append(grid, list(expand.grid(.C = c(.50, 1))))
-names(grid) <- c("plsda", "svm")
