538a79ad966ccf2023559477e059c5abc516a031
Charles Determan <cdeterman@healthgrades.com>
2017-01-04 09:54:20 -0600

    newest assertive package changed some error messages that crashed a few tests, fixed by updating regex

--- tests/testthat/test_stability_metrics.R
+++ tests/testthat/test_stability_metrics.R
@@ -106,9 +106,9 @@ test_that("pairwise.model.stability requires appropriate inputs", {
 })
 
 test_that("RPT requires appropriate inputs", {
-    expect_error(RPT("A", 0.80), "stability is not of type 'numeric'")
-    expect_error(RPT(0.80, "B"), "performance is not of type 'numeric'")
-    expect_error(RPT(0.80,0.80,"C"), "beta is not of type 'numeric'")
+    expect_error(RPT("A", 0.80), "stability is not of class 'numeric'")
+    expect_error(RPT(0.80, "B"), "performance is not of class 'numeric'")
+    expect_error(RPT(0.80,0.80,"C"), "beta is not of class 'numeric'")
     expect_error(RPT(1.1,.80), "stability is not in range 0 to 1.")
     expect_error(RPT(.80,1.1), "performance is not in range 0 to 1.")
     expect_error(RPT(.80,.80,-1), "beta is non-positive.")
